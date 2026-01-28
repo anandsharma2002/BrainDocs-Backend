01ac810e007c56f6f9d37786556664cf66b581c3
Anand Sharma <anandsharma.ts3000@gmail.com>
2026-01-29 00:34:10 +0530

    Update CORS for production

--- server.js
+++ server.js
@@ -13,8 +13,13 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000", "http://localhost:3001"],
-        methods: ["GET", "POST", "PUT", "DELETE"]
+        origin: [
+            "http://localhost:3000",
+            "http://localhost:3001",
+            "https://anandsharma2002.github.io"
+        ],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
     }
 });
 
